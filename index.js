4074115b88d706e1b551c82040acfd14a2ab870e
Shivam Jha <108573467+ShivamKumarJha2004@users.noreply.github.com>
2024-08-03 16:40:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors()),
 app.use(express.json());
 
 dotenv.config();
-const port=process.env.port||30001;
+const port=process.env.port||3003;
 const url=process.env.MONGODBURL;
 
 // connect to mongodb
